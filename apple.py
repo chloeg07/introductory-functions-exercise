c5170bc713ab1d3e4b3625007cd15aac3dbdd32e
Chloe <144443749+chloeg07@users.noreply.github.com>
2024-03-08 12:17:26 +0000

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,7 @@
+def Add_Two  (a,b):
+    total = a + b
+    return total
+
+def Un_Adder (a):
+    wordnew = "un" + a
+    return wordnew
---
+++ appleuse.py
@@ -0,0 +1,10 @@
+import apple
+
+inp1 = int(input("Enter a number   "))
+inp2 = int(input("Enter a second number   "))
+result = apple.Add_Two(inp1,inp2)
+print(result)
+
+word = input("Write a word  ")
+result = apple.Un_Adder(word)
+print(result)
